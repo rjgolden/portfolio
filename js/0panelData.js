f36405ab708f8b81832f4732810c32df64e43540
rjgolden <ryanjgolden00@gmail.com>
2026-04-21 12:52:39 -0400

    Redeploy

--- js/0panelData.js
+++ js/0panelData.js
@@ -29,11 +29,11 @@ const panelData = [
     title: "Resume",
     body: `
       <div class="resume-preview">
-        <img src="resources/resume.png" alt="Resume preview">
+        <img src="/portfolio/resources/resume.png" alt="Resume preview">
       </div>
     `,
     links: [
-      { label: "Open Resume", url: "resources/resume.pdf" }
+      { label: "Open Resume", url: "/portfolio/resources/resume.pdf" }
     ]
   },
   {
