8ec8e2c5301f6021c9e49a813a0e0bb895d40160
Ryan <ryanjgolden00@gmail.com>
2026-05-09 23:40:12 -0400

    small changes

--- js/0panelData.js
+++ js/0panelData.js
@@ -13,35 +13,27 @@ const feed1 = `<p>First blog post content goes here.</p>`;
 const feed2 = `<p>Second blog post content goes here.</p>`;
 
 const about = `<p>Welcome to my portfolio! My name is Ryan Golden. I am an aspiring software developer seeking a career coding in C++. I graduated from the University of Rhode Island with a Bachelor of 
-      Science in Computer Science. I have a strong passion for game development, which I pursue as a hobby in my free time. I enjoy working with C++ due to its performance and its widespread use 
-      in fields that dominate software around the globe. I am particularly interested in systems programming and performance-critical applications. This portfolio is a glimpse at the kind of clean and performant UI/UX I will be focused on for my projects.
-      <br> <br>Please feel free to contact me through one of the various sources on the contact page if you are interested in working together or have any questions.
-      <br> about
-      <br> about
-      <br> about
-      <br> about
-      <br> about
-      <br> about
-      <br> about
-      <br> about
-      <br> about
-      <br> about
-      <br> about
-      </p>`;
+      Science in Computer Science. </p>
+
+      <p> This portfolio is based on a combination of both the GameCube and Ps2 startup animations. These were two of my favorite consoles growing up and are the host of the now classic
+      games I played in my youth, so I wanted to try to capture the distopian 2000s futuristic ambiance vibe. The kind of game development that was responsible for these games is also resposible for my interest in game development as a career. </p>
+
+      <p> Other than making cool websites, I have a strong interest in game development, which I pursue as a hobby in my free time. I enjoy working with C++ due to its performance and its widespread use 
+      in fields that dominate software around the globe. I am particularly interested in systems programming and performance-critical applications. This portfolio is a glimpse at the kind of clean and performant UI/UX I will be focused on for my projects. </p>
+      <p> Please feel free to contact me through one of the various sources on the contact page if you are interested in working together or have any questions! </p>`;
 
 const skills = `<p>•Programming Languages: C, C++, Python, Javascript, Typescript, Lua <br><br>
-      •Frameworks & Libraries: Raylib, Node.js, Three.js, Flutter<br><br>
-      •Database & Technologies: SQLite, MySQL, Oracle APEX, Google Firebase<br><br>
-      •Development & Design Tools: Git, Github, GDB, Aseprite, Tiled, Figma, Pico-8<br><br></p>
-    `
+                   •Frameworks & Libraries: Raylib, Node.js, Three.js, Flutter<br><br>
+                   •Database & Technologies: SQLite, MySQL, Oracle APEX, Google Firebase<br><br>
+                   •Development & Design Tools: Git, Github, GDB, Aseprite, Tiled, Figma, Pico-8<br><br></p>`
     
 const panelData = [
   {
-    title: "About Me",
+    title: "About",
     body: ``,
     links: [
-      { label: "About", project: about },
-      { label: "Skills", project: skills }
+      { label: "About Me", project: about },
+      { label: "My Skills", project: skills }
     ]
   },
 
@@ -55,9 +47,12 @@ const panelData = [
     title: "Projects",
     body: ``,
     links: [
-      { label: "Project 1", project: project1 },
-      { label: "Project 2", project: project2 },
-      { label: "Project 3", project: project3 }
+      { label: "WalkPoint URI", project: project1 },
+      { label: "Raylib Shooter", project: project2 },
+      { label: "Inklings Project", project: project3 },
+      { label: "Floppy Fish", project: project3 },
+      { label: "Pong", project: project3 },
+      { label: "Wordle Bot", project: project3 }
     ]
   },
 
