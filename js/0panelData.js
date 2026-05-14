b2acee71bb28dd45f9b3a012ef5f93ed2f4cba44
rjgolden <ryanjgolden00@gmail.com>
2026-05-14 11:27:41 -0400

    Mobile viewing fix

--- js/0panelData.js
+++ js/0panelData.js
@@ -12,28 +12,28 @@ const project3 = `<p>Third project content goes here.</p>`;
 const feed1 = `<p>First blog post content goes here.</p>`;
 const feed2 = `<p>Second blog post content goes here.</p>`;
 
-const about = `<p>Welcome to my portfolio! My name is Ryan Golden. I am an aspiring software developer seeking a career coding in C++. I graduated from the University of Rhode Island with a Bachelor of 
+const about = `<p class = "indentp">Welcome to my portfolio! My name is Ryan Golden. I am an aspiring software developer seeking a career coding in C++. I graduated from the University of Rhode Island with a Bachelor of 
       Science in Computer Science. </p>
 
-      <p> This portfolio is based on a combination of both the GameCube and Ps2 startup animations. These were two of my favorite consoles growing up and are the host of the now classic
+      <p class = "indentp"> This portfolio is based on a combination of both the GameCube and Ps2 startup animations. These were two of my favorite consoles growing up and are the host of the now classic
       games I played in my youth, so I wanted to try to capture the distopian 2000s futuristic ambiance vibe. The kind of game development that was responsible for these games is also resposible for my interest in game development as a career. </p>
 
-      <p> Other than making cool websites, I have a strong interest in game development, which I pursue as a hobby in my free time. I enjoy working with C++ due to its performance and its widespread use 
+      <p class = "indentp"> Other than making cool websites, I have a strong interest in game development, which I pursue as a hobby in my free time. I enjoy working with C++ due to its performance and its widespread use 
       in fields that dominate software around the globe. I am particularly interested in systems programming and performance-critical applications. This portfolio is a glimpse at the kind of clean and performant UI/UX I will be focused on for my projects. </p>
-      <p> Please feel free to contact me through one of the various sources on the contact page if you are interested in working together or have any questions! </p>`;
 
-const skills = `<p>•Programming Languages: C, C++, Python, Javascript, Typescript, Lua <br><br>
-                   •Frameworks & Libraries: Raylib, Node.js, Three.js, Flutter<br><br>
-                   •Database & Technologies: SQLite, MySQL, Oracle APEX, Google Firebase<br><br>
-                   •Development & Design Tools: Git, Github, GDB, Aseprite, Tiled, Figma, Pico-8<br><br></p>`
+      <p class = "indentp"> Please feel free to contact me through one of the various sources on the contact page if you are interested in working together or have any questions! </p>`;
+
+const skills = `<p><b>Programming Languages:</b> C, C++, Python, Javascript, Typescript, Lua <br><br>
+                   <b>Frameworks & Libraries:</b> Raylib, Node.js, Three.js, Flutter<br><br>
+                   <b>Database & Technologies:</b> SQLite, MySQL, Oracle APEX, Google Firebase<br><br>
+                   <b>Development & Design Tools:</b> Git, Github, GDB, Aseprite, Tiled, Figma, Pico-8<br><br></p>`
     
 const panelData = [
   {
     title: "About",
-    body: ``,
+    body: about,
     links: [
-      { label: "About Me", project: about },
-      { label: "My Skills", project: skills }
+      { label: "Skills", project: skills }
     ]
   },
 
