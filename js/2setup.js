a071e13e608bfcf1aa4f60c6a33e3ad694f8422f
rjgolden <ryanjgolden00@gmail.com>
2026-05-26 15:07:05 -0400

    Updated mobile positioning

--- js/2setup.js
+++ js/2setup.js
@@ -46,7 +46,7 @@ function drawFace(face, color, isActive) {
   if (isActive) {
     ctx.shadowColor = color;
 
-    if(lightModeEnabled && isMobileViewport) ctx.shadowBlur = 45;
+    if(lightModeEnabled && isMobileViewport) ctx.shadowBlur = 25;
     else ctx.shadowBlur = 60;
 
     ctx.fillStyle = color;
