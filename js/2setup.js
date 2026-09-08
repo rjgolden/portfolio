ae398ae4b04f6c5b6b0c6d74137608b56b80f6a5
Ryan <ryanjgolden00@gmail.com>
2026-09-08 16:01:37 -0400

    Fixed glow

--- js/2setup.js
+++ js/2setup.js
@@ -44,14 +44,13 @@ function drawFace(face, color, isActive) {
   // if the panel is facing the screen
   if (isActive) {
     ctx.shadowColor = color;
-    if(isCompatactLayout()) {
-      x.shadowBlur = 10;
+    if(isCompactLayout() === true) {
+      ctx.shadowBlur = 10;
     }
     else {
-      x.shadowBlur = 35;
+      ctx.shadowBlur = 35;
     }
     ctx.fillStyle = color;
-    ctx.globalAlpha = 0.25;
     ctx.fillText(label, canvas.width / 2, canvas.height / 2);
     ctx.globalAlpha = 0.75;
   } else {
